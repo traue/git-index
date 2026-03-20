1796e26f492bfc2c500a09432e24b96bae1ffcbe
Thiago G. Traue <t.traue@gmail.com>
2026-03-20 18:02:26 -0300

    Update configs.js

--- js/configs.js
+++ js/configs.js
@@ -1,5 +1,5 @@
 var configs = {
-    version: "3.3.0",
+    version: "3.3.1",
     apiURL: "https://api.traue.com.br/disciplinas/",
     gitURL: "https://github.com/traue/",
     turnosKey: "turnos"
