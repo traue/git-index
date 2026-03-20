58a0b6ab7d1f5e2e74a76b842a89ee09bf0fd65b
Thiago G. Traue <t.traue@gmail.com>
2026-03-20 18:21:17 -0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,8 +1,10 @@
 <?php
 
-    header("Access-Control-Allow-Origin: https://git.traue.com.br");
+    header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: GET, OPTIONS");
     header("Access-Control-Allow-Headers: Content-Type, Accept");
+    header("Cache-Control: no-store, no-cache, must-revalidate");
+    header("Vary: Origin");
 
     if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
         http_response_code(204);
